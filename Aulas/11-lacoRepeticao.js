71ad8563aeea74b26af0b8d6ffbd0d6f4ccee555
Ricci201 <lucasrricci2008@gmail.com>
2025-02-20 14:43:51 -0300

    Laços de Repetição

---
+++ Aulas/11-lacoRepeticao.js
@@ -0,0 +1,6 @@
+//Laço de repetição - FOR
+
+// O professor te colocou de castigo e pediu para você escrever mil vezes a frase "Eu vou prestar atenção às aulas"
+for (let i = 0; i < 1000; i++) {
+  console.log("Eu vou prestar atenção às aulas");
+}
